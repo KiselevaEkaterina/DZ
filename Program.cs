5b6f94787e23c6fdb4f8b9add112d466abed1bb3
KiselevaEkaterina <122477920+KiselevaEkaterina@users.noreply.github.com>
2023-02-06 09:40:55 +0300

    Zadacha 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// программа которая на вход принимает 2 числа и на выходе показывает какое больше и меньше.
+
+Console.Write("Ведите первое число: ");
+int a=int.Parse(Console.ReadLine()!);
+
+Console.Write("Ведите второе число: ");
+int b=int.Parse(Console.ReadLine()!);
+
+if (a>b)
+{    
+    Console.WriteLine($"Большее число {a}");
+    Console.WriteLine($"Меньшее число {b}");
+    
+}
+else
+{
+    Console.WriteLine($"Большее число {b}");
+    Console.WriteLine($"Меньшее число {a}");
+}
