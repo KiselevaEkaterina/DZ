31ea707c4c72d7842f32b5327ab34aabb71963ca
KiselevaEkaterina <122477920+KiselevaEkaterina@users.noreply.github.com>
2023-02-06 09:52:32 +0300

    Add files via upload
    задача 23

--- Program.cs
+++ Program.cs
@@ -1,22 +1,12 @@
-﻿// Задача 21.
+﻿// Задача 23
 
-Console.WriteLine($"Введите X1: ");
-int x1=int.Parse(Console.ReadLine()!);
-Console.WriteLine($"Введите Y1: ");
-int y1=int.Parse(Console.ReadLine()!);
-Console.WriteLine($"Введите Z1: ");
-int z1=int.Parse(Console.ReadLine()!);
+Console.Write($"Введите число: ");
+int n=int.Parse(Console.ReadLine()!);
+int count=1;
+while (count<=n)
+{
+    Console.WriteLine($"{Math.Pow(count,3)}");
 
-Console.WriteLine($"Введите X2: ");
-int x2= int.Parse(Console.ReadLine()!);
-Console.WriteLine($"Введите Y2: ");
-int y2=int.Parse(Console.ReadLine()!);
-Console.WriteLine($"Введите Z2: ");
-int z2=int.Parse(Console.ReadLine()!);
-
-double x3= Math.Pow(x2-x1,2);
-double y3= Math.Pow(y2-y1,2);
-double z3= Math.Pow(z2-z1,2);
-double Line= Math.Sqrt(x3+y3+z3);
-Console.WriteLine($"Расстояние от точки А ({x1},{y1},{z1}) до точки В ({x2},{y2},{z2}) равно {Line:F2}");
-
+    if (count<1) Console.Write (", ");
+    count++; 
+}
